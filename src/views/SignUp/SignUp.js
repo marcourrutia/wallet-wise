5854e916edfaee7430b4553c003b40005b10801c
marcourrutia <m.urrutia1991@gmail.com>
2024-09-11 21:35:10 -0300

    form

--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -11,6 +11,29 @@ export const SignUp = () => {
         <BtnBack />
       </div>
       <h1 className="signup-h1">Sign up for free</h1>
+      <div class="divider">
+        <span>OR</span>
+      </div>
+      <form className="signup-form">
+        <div className="inputs-container">
+          <div className="label-input-contain">
+            <label for="inputEmail">Email</label>
+            <input id="inputEmail" />
+          </div>
+          <div className="label-input-contain">
+            <label for="inputFirstName">First Name</label>
+            <input id="inputFirstName" />
+          </div>
+          <div className="label-input-contain">
+            <label for="inputLastName">Email</label>
+            <input id="inputLastName" />
+          </div>
+          <div className="label-input-contain">
+            <label for="inputPassword">Email</label>
+            <input id="inputPassword" />
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
