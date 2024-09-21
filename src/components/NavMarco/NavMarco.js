d323496ed16b8b1c01627a173aa1619c2405d866
marcourrutia <m.urrutia1991@gmail.com>
2024-09-21 18:56:02 -0300

    avance y correcciones

--- src/components/NavMarco/NavMarco.js
+++ src/components/NavMarco/NavMarco.js
@@ -1,7 +1,6 @@
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { MainLogo } from "../MainLogo/MainLogo";
 import "./NavMarco.css";
-import { SignOutButton, useUser } from "@clerk/clerk-react";
 
 export const NavMarco = () => {
   const navigate = useNavigate();
@@ -12,51 +11,26 @@ export const NavMarco = () => {
     });
   };
 
-  const { user, isSignedIn, isLoaded } = useUser();
-
-  const location = useLocation();
-  const isLoginView = location.pathname === "/login";
-  const isSignupView = location.pathname === "/signup";
-
-  if ((isLoginView || isSignupView) && !isSignedIn) {
-    return null;
-  }
-
   return (
     <nav className="nav-container">
       <MainLogo />
-      {isSignedIn ? (
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <span className="span-login" style={{ marginRight: 10 }}>
-            Hello, {user?.firstName}!
-          </span>
-          <SignOutButton />
-        </div>
-      ) : (
-        <div className="nav-btns-container">
-          <span className="login-span">Support</span>
-          <div className="dropdown">
-            <span className="login-span">About us</span>
-            <div className="dropdown-content">
-              <span onClick={() => scrollToSection("mision")}>Mision</span>
-              <span onClick={() => scrollToSection("vision")}>Vision</span>
-            </div>
+      <div className="nav-btns-container">
+        <span className="login-span">Support</span>
+        <div className="dropdown">
+          <span className="login-span">About us</span>
+          <div className="dropdown-content">
+            <span onClick={() => scrollToSection("mision")}>Mision</span>
+            <span onClick={() => scrollToSection("vision")}>Vision</span>
           </div>
-          <span className="login-span">Services</span>
-          <button
-            className="signup-btn-form"
-            onClick={() => navigate("/signup")}
-          >
-            Sign up
-          </button>
-          <button
-            className="signup-btn-form"
-            onClick={() => navigate("/login")}
-          >
-            Log in
-          </button>
         </div>
-      )}
+        <span className="login-span">Services</span>
+        <button className="signup-btn-form" onClick={() => navigate("/signup")}>
+          Sign up
+        </button>
+        <button className="signup-btn-form" onClick={() => navigate("/login")}>
+          Log in
+        </button>
+      </div>
     </nav>
   );
 };
