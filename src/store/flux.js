4011c9a1b794440a56555547b68b21417a9a1bc3
Maryanni <52219648+Maryanni@users.noreply.github.com>
2024-09-21 21:18:49 -0300

    Login google (#6)
    * First change
    
    * First change
    
    * Second change
    
    * Third change
    
    * Fourth change
    
    * First update

--- src/store/flux.js
+++ src/store/flux.js
@@ -1,7 +1,38 @@
+
 const getState = ({ getActions, getStore, setStore }) => {
   return {
-    store: {},
-    actions: {},
+    store: {
+      first_name: [],
+      last_name: [],
+      email: [] 
+    },
+    actions: {
+      postToken: async (firstName, lastName, email) => {
+        try {
+          console.log(lastName);
+          const response = await fetch("http://localhost:5050/login_google", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              first_name: firstName,
+              last_name: lastName,
+              email: email 
+            }),
+          });
+
+          if (!response.ok) {
+            throw new Error("There is an error");
+          }
+        
+          const data = await response.json();
+          console.log("Respuesta del backend:", data);
+        } catch (error) {
+          console.error("Error al enviar el token:", error);
+        }
+      },
+    },
   };
 };
 
