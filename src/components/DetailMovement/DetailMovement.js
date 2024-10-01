809927006539dff886aa5e335d1979d9e5c2ab4c
Maryanni <marziliano14n@gmail.com>
2024-10-01 13:12:58 -0300

    Update list

--- src/components/DetailMovement/DetailMovement.js
+++ src/components/DetailMovement/DetailMovement.js
@@ -1,9 +1,18 @@
 import { Context } from "../../store/context";
 import "./DetailMovement.css";
 import React, { useContext, useEffect } from "react";
+import { ImCircleUp } from "react-icons/im";
 
 export const DetailMovement = ({ accountId }) => {
   const state = useContext(Context);
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+  };
 
   console.log(accountId);
 
@@ -16,26 +25,73 @@ export const DetailMovement = ({ accountId }) => {
   return (
     <div className="container mt-4 mb-4">
       <div className="style-movement">
-
+        <table className="table style-table-head mb-1">
+          <thead>
+            <tr>
+              <th scope="col">Category</th>
+              <th scope="col">Amount</th>
+              <th scope="col">Date of transaction</th>
+            </tr>
+          </thead>
+        </table>
         {state.store.categorySave.map((category, index) => {
+          const collapseId = `collapse-${index}`;
+          let totalAmount = 0;
           return (
-            <div>
-              <h2>{category.name}</h2>
-              {state.store.movementByAccount.map((item, index) => {
-                if (item.category == category.name) {
-                  return (
-                    <table className="table table-body-transaction">
-                      <tbody>
-                        <tr>
-                          <th scope="col">{item.transaction}</th>
-                          <td>{item?.amount}</td>
-                          <td>{item?.transaction_date}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  );
-                }
-              })}
+            <div className="accordion-item accordion-style" key={index}>
+              <h2 className="accordion-header">
+                <button
+                  className="accordion-button button-style-category"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target={`#${collapseId}`}
+                  aria-expanded="true"
+                  aria-controls="panelsStayOpen-collapseOne"
+                >
+                  <div className="icon-accordion">
+                    {category.name}
+                    <ImCircleUp className={`icon-colap ${collapseId}`} />
+                  </div>
+                </button>
+              </h2>
+              <div id={collapseId} className="accordion-collapse collapse show">
+                <div className="accordion-body">
+                  {state.store.movementByAccount.map((item, index2) => {
+                    if (item.category == category.name) {
+                      totalAmount += item.amount;
+                      const formattedAmount = item.amount.toLocaleString(
+                        "es-CL",
+                        {
+                          minimumFractionDigits: 2,
+                          maximumFractionDigits: 2,
+                        }
+                      );
+                      return (
+                        <table
+                          className="table table-body-transaction"
+                          key={index2}
+                        >
+                          <tbody>
+                            <tr>
+                              <th scope="col">{item.transaction}</th>
+                              <td>{formattedAmount}</td>
+                              <td>{formatDate(item?.transaction_date)}</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      );
+                    }
+                  })}{" "}
+                  <h4>
+                    Total:{" "}
+                    {totalAmount.toLocaleString("es-CL", {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}{" "}
+                    CLP
+                  </h4>
+                </div>
+              </div>
             </div>
           );
         })}
