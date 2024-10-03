de0d57c9a36abde425506a9931282348a871d180
Maryanni <marziliano14n@gmail.com>
2024-10-02 23:46:33 -0300

    Change chat

--- src/routerConfig.js
+++ src/routerConfig.js
@@ -54,7 +54,7 @@ export const routes = [
     element: (
       <PrivateRoute>
         <Layout>
-          <Maintainer />
+          <FormMaintainer />
         </Layout>
       </PrivateRoute>
     ),
