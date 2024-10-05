8f38282b95cb6e85e5d8ce4308d118f6fab17641
Maryanni <52219648+Maryanni@users.noreply.github.com>
2024-10-05 17:36:45 -0300

    First change expense tracker (#16)

--- src/components/ExpenseSumary/ExpenseSumary.js
+++ src/components/ExpenseSumary/ExpenseSumary.js
@@ -1,6 +1,15 @@
 import "./ExpenseSumary.css";
 
-export const ExpenseSumary = () => {
+export const ExpenseSumary = ({ totalIncome, totalExpense }) => {
+  const formattedIncome = totalIncome.toLocaleString("es-CL", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
+  const formattedExpense = totalExpense.toLocaleString("es-CL", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
   return (
     <div className="container mt-4 mb-4">
       <div className="div-sumary">
@@ -11,11 +20,20 @@ export const ExpenseSumary = () => {
         <form className="row g-3 form-sumary">
           <div className="col-md-6 div-field-sumary">
             <div className="d-flex align-items-end">
-              <label htmlFor="input-ingreso" className="form-label me-3">
+              <label
+                htmlFor="input-ingreso"
+                className="form-label me-3 label-sumary"
+              >
                 Total Income
               </label>
             </div>
-            <input type="number" className="input-sumary" id="input-ingreso" disabled/>
+            <input
+              type="text"
+              className="input-sumary"
+              id="input-ingreso"
+              disabled
+              value={formattedIncome}
+            />
           </div>
 
           <div className="col-md-6 div-field-sumary">
@@ -25,7 +43,13 @@ export const ExpenseSumary = () => {
             >
               Total Expenses
             </label>
-            <input type="number" className="input-sumary" id="input-gasto" disabled/>
+            <input
+              type="text"
+              className="input-sumary"
+              id="input-gasto"
+              disabled
+              value={formattedExpense}
+            />
           </div>
         </form>
       </div>
