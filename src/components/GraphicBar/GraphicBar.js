758e3d016ad33a5fcf23fcdd4a401c28b9964d1f
marcourrutia <m.urrutia1991@gmail.com>
2024-10-15 00:28:06 -0300

    grafico ok

--- src/components/GraphicBar/GraphicBar.js
+++ src/components/GraphicBar/GraphicBar.js
@@ -50,7 +50,7 @@ export const GraphicBar = () => {
           default:
             break;
         }
-      });
+      }, []);
 
       const formattedData = Object.values(tempData)
         .sort((a, b) => {
@@ -79,35 +79,38 @@ export const GraphicBar = () => {
   return (
     <div className="graphic-bar-container">
       {data.length > 0 ? (
-        <ResponsiveContainer width="100%" aspect={2}>
-          <BarChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid stroke="#ccc" strokeDasharray="1" />
-            <XAxis dataKey="name" stroke="#f7fbfe" />
-            <YAxis stroke="#f7fbfe" />
-            <Tooltip />
-            <Legend />
-            <Bar
-              dataKey="incomes"
-              fill="#8884d8"
-              activeBar={<Rectangle fill="pink" stroke="blue" />}
-            />
-            <Bar
-              dataKey="expenses"
-              fill="#82ca9d"
-              activeBar={<Rectangle fill="gold" stroke="purple" />}
-            />
-          </BarChart>
-        </ResponsiveContainer>
+        <>
+          <h3>Year: {year}</h3>
+          <ResponsiveContainer width="100%" aspect={2}>
+            <BarChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid stroke="#ccc" strokeDasharray="1" />
+              <XAxis dataKey="name" stroke="#f7fbfe" />
+              <YAxis stroke="#f7fbfe" />
+              <Tooltip />
+              <Legend />
+              <Bar
+                dataKey="incomes"
+                fill="#8884d8"
+                activeBar={<Rectangle fill="pink" stroke="blue" />}
+              />
+              <Bar
+                dataKey="expenses"
+                fill="#82ca9d"
+                activeBar={<Rectangle fill="gold" stroke="purple" />}
+              />
+            </BarChart>
+          </ResponsiveContainer>
+        </>
       ) : (
         <h4>No flows...</h4>
       )}
